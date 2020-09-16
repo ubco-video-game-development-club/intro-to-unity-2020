deab5f43a8b53f4d7f9f05b7a47f637240670efb
Jaden Balogh <jadenbalogh@gmail.com>
2020-09-15 22:42:12 -0700

    Add project files

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject cubePrefab;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello from Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get horizontal user input (-1 to 1)
+        float xInput = Input.GetAxis("Horizontal");
+
+        // Change the position on the x axis based on the user input
+        transform.position += new Vector3(xInput * speed * Time.deltaTime, 0, 0);
+
+        // Get left click input
+        bool isMouseDown = Input.GetButtonDown("Fire1");
+        if (isMouseDown)
+        {
+            // Spawn a cube!
+            Instantiate(cubePrefab, new Vector3(0, 5, 0), Quaternion.AngleAxis(45f, Vector3.forward));
+        }
+    }
+}
---
+++ Assets/Scripts/TriggerExample.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerExample : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("Got em!");
+    }
+}
